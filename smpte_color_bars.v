ef9d0bbd238002ca4a0501ea2dfc9309ec9390ac
gwholtha1 <47867003+gwholtha1@users.noreply.github.com>
2019-05-15 12:03:50 -0700

    Create smpte_color_bars.v
    Module declaration, hvsync_gen instantiation, and some comments about the color bars

---
+++ smpte_color_bars.v
@@ -0,0 +1,37 @@
+`include "hvsync_generator.v"
+
+module smpte_color_bars(
+  input clk,
+  input reset,
+  output hsync,
+  output vsync,
+  output [2:0] rgb
+);
+
+wire display_on; // Indicates when we are in the visible portion of the display
+wire [8:0] hpos; // Horizontal position of the electron beam
+wire [8:0] vpos; // Vertical position of the electron beam
+
+hvsync_generator hvsync_gen (
+  .clk(clk),
+  .reset(reset),
+  .hsync(hsync),
+  .vsync(vsync),
+  .display_on(display_on),
+  .hpos(hpos),
+  .vpos(vpos)
+);
+
+// For SMPTE color bars, divide the visible screen into 7 vertical bars
+// White, yellow, cyan, green, magenta, red, blue
+// Table below shows when red, green, and blue guns should be on (1) or off (0)
+//
+// This simple one-bit setup can be translated to 75% luminosity in hardware,
+// which is a typical level for these color bars
+//
+// Bar:   0 1 2 3 4 5 6
+//
+// Red:   1 1 0 0 1 1 0
+// Green: 1 1 1 1 0 0 0
+// Blue:  1 0 1 0 1 0 1
+
