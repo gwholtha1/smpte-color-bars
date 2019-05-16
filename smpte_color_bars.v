5668f7f155477560ac5438a3ccc320c4c8ebc5a4
gwholtha1 <47867003+gwholtha1@users.noreply.github.com>
2019-05-16 11:52:57 -0700

    Change bar width calculation
    Bar width calculation now based on hpos counter bits

--- smpte_color_bars.v
+++ smpte_color_bars.v
@@ -13,9 +13,9 @@ wire [8:0] hpos; // Horizontal position of the electron beam
 wire [8:0] vpos; // Vertical position of the electron beam
 
 reg [5:0] b_clk; // Clock signal to determine blue gun timing
-reg r_on; // signal to drive red gun
-reg g_on; // signal to drive green gun
-reg b_on; // signal to drive blue gun
+wire r_on; // signal to drive red gun
+wire g_on; // signal to drive green gun
+wire b_on; // signal to drive blue gun
 
 hvsync_generator hvsync_gen (
   .clk(clk),
@@ -40,50 +40,13 @@ hvsync_generator hvsync_gen (
 // Green: 1 1 1 1 0 0 0
 // Blue:  1 0 1 0 1 0 1
 //
-parameter BAR_WIDTH = H_DISPLAY / 7; // Width of one vertical bar
-
-// Start a new scanline every time the horizontal position counter is reset
-assign new_scanline = hpos == 9'b0 && display_on;
-  
-assign rgb = {r_on, g_on, b_on};
-  
 // RGB signals can be controlled by three levels of divided clocks
   
 // Consider switching the blue gun every 32 clocks. This makes it easy to pick off
 // bits of hpos to determine the r_on, g_on, and b_on signals.
-
-  always @(posedge clk) begin
-    if (reset) begin
-      r_on <= 1;
-      g_on <= 1;
-      b_on <= 1;
-      b_clk <= 6'b0;
-    end else if (b_clk == BAR_WIDTH - 1) begin
-      b_on <= ~b_on;
-      b_clk <= 6'b0;
-    end else begin
-      b_clk <= b_clk + 1;
-    end
-  end
   
-  always @(posedge display_on) begin
-    // Turn on all three guns when starting a new scanline
-    r_on <= 1;
-    g_on <= 1;
-    b_on <= 1;
-  end
-
-  always @(posedge b_on) begin
-    // Turn on and off red gun at half the frequency of blue gun
-    r_on <= ~r_on;
-  end
+  assign b_on = ~hpos[5] && display_on;
+  assign r_on = ~hpos[6] && display_on;
+  assign g_on = ~hpos[7] && display_on;
   
-  always @(posedge r_on) begin
-    // Turn on and off green gun at half the frequency of red gun
-    g_on = ~g_on;
-  end
-    
-  endmodule
-    
-  
-
+assign rgb = {r_on, g_on, b_on};
